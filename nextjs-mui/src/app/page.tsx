8de525bb699cc6e74782209361f31a680435b567
stswoon <xx@xx.xx>
2026-01-29 14:31:10 +0400

    init next+mui

--- nextjs-mui/src/app/page.tsx
+++ nextjs-mui/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "@mui/material/Button";
+import Link from './Link';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      Hello World
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <Button component={Link} href="/about" variant="contained">
+                Go to About Page
+            </Button>
+        </div>
+    );
 }
