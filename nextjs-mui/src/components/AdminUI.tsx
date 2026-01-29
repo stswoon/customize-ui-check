ab5c07ecbea73cb2c992f96af98c21eefc6f0298
stswoon <xx@xx.xx>
2026-01-29 16:51:43 +0400

    admin ui

--- nextjs-mui/src/components/AdminUI.tsx
+++ nextjs-mui/src/components/AdminUI.tsx
@@ -1,3 +1,30 @@
+import MenuItem from "@mui/material/MenuItem";
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+
 export default function AdminUI() {
-    return <div>AdminUI</div>
+    return <div>
+        <Select>
+            <MenuItem value={""}>Button Color</MenuItem>
+            <MenuItem value={"red"}>red</MenuItem>
+            <MenuItem value={"pink"}>pink</MenuItem>
+            <MenuItem value={"green"}>green</MenuItem>
+            <MenuItem value={"gray"}>gray</MenuItem>
+        </Select>
+
+        <Select>
+            <MenuItem value={""}>Button Size</MenuItem>
+            <MenuItem value={"sm"}>sm</MenuItem>
+            <MenuItem value={"xl"}>xl</MenuItem>
+            <MenuItem value={"xxl"}>xxl</MenuItem>
+            <MenuItem value={"xxxl"}>xxxl</MenuItem>
+        </Select>
+
+        <Select>
+            <MenuItem value={""}>Card Title Size</MenuItem>
+            <MenuItem value={"sm"}>sm</MenuItem>
+            <MenuItem value={"xl"}>xl</MenuItem>
+            <MenuItem value={"xxl"}>xxl</MenuItem>
+            <MenuItem value={"xxxl"}>xxxl</MenuItem>
+        </Select>
+    </div>
 }
